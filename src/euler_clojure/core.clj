6d0ad58ba3a15e0c735685d8a43b19ad24fa0680
clbs <chrislbs@gmail.com>
2014-02-22 02:06:10 -0600

    Adding code to permit passing problem number to main to run problem.

--- src/euler_clojure/core.clj
+++ src/euler_clojure/core.clj
@@ -4,4 +4,13 @@
 
 (defn -main
   [& args]
-  (println args))
+  (for
+      [ problem-number args
+        :when (number? problem-number)]
+    (let [ problem-str (format "euler-clojure.problem%d" problem-number)
+           problem-ns (symbol problem-str)
+           problem-fn (symbol (format "%s/run" problem-str))]
+      (require problem-ns)
+      (println (format "Running problem %d :" problem-number))
+      ((resolve problem-fn))
+      )))
