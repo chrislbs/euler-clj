3db3ebb27d17d589f2517670c83e2f272c0c52ba
clbs <chrislbs@gmail.com>
2014-02-22 00:46:58 -0600

    removing use of atoms from solution.

--- src/euler_clojure/core.clj
+++ src/euler_clojure/core.clj
@@ -3,14 +3,14 @@
 
 
 (defn is-palindrome? [number]
-  (def value (atom (str number)))
-  (def a (atom (vec (seq @value))))
-  (def b (atom (vec (reverse @a))))
+  (def value (str number))
+  (def a (vec (seq value)))
+  (def b (vec (reverse a)))
 
   (every? #(true? %) 
     (map = 
-         (subvec @a 0 (/ (count @a) 2))
-         (subvec @b 0 (/ (count @b) 2)))))
+         (subvec a 0 (/ (count a) 2))
+         (subvec b 0 (/ (count b) 2)))))
 
 (defn -main
   [& args]
