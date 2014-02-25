f8fff250477f823168d1359629f430664691aa03
Chris Pounds <chrislbs@gmail.com>
2014-02-24 18:43:51 -0600

    Adding a method to generate the lcm of n numbers.

--- src/euler_clojure/math/util.clj
+++ src/euler_clojure/math/util.clj
@@ -1,4 +1,6 @@
-(ns euler-clojure.math.util)
+(ns euler-clojure.math.util
+  (:require [euler-clojure.math.prime :as prime])
+  (:require [clojure.math.numeric-tower :as clj-math]))
 
 ; euclid's algorithm for solving GCD
 ; (http://en.wikipedia.org/wiki/Greatest_common_divisor)
@@ -9,6 +11,32 @@
       a
       (recur b, (mod a b)))))
 
-; least common multiples of two numbers
-(defn lcm [a b]
-  (/ (Math/abs (* a b)) (gcd a b)))
+; get the most occurring factors from a collection of prime-factor-maps
+(defn most-occuring-factors [factor-map-coll]
+  (reduce
+    (fn [highest-occurrence-map current-factor-map]
+      (merge highest-occurrence-map
+             (filter
+               (fn [kvp]
+                 (let [prime-num (get kvp 0)
+                       prime-count (get kvp 1)]
+                   (or
+                     (not (contains? highest-occurrence-map prime-num))
+                     (< (get highest-occurrence-map prime-num) prime-count))))
+               current-factor-map)))
+    {}
+    factor-map-coll))
+
+; least common multiple
+(defn lcm
+  ([a b] (clj-math/lcm a b))
+  ([numbers]
+   (reduce
+     (fn [result kvp]
+       (* result (clj-math/expt (get kvp 0) (get kvp 1))))
+     1
+     (most-occuring-factors
+       (map
+         prime/prime-factor-map
+         numbers)))))
+
