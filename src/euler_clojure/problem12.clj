4eafbc9701f1cea34c47c3245354bfeba95120fb
clbs <chrislbs@gmail.com>
2014-02-27 01:36:37 -0600

    Adding faster divisor check function after solving

--- src/euler_clojure/problem12.clj
+++ src/euler_clojure/problem12.clj
@@ -12,11 +12,15 @@
           (combo/subsets (math-prime/prime-factors num)))))
     1 ))
 
+(defn num-divisors [num]
+  (* 2 (count (filter #(zero? (rem num %)) (range 1 (Math/sqrt num))))))
+
 (def triangle-numbers
   (map first (iterate (fn [[sum num]] [(+ sum (inc num)) (inc num)]) [1 1])))
 
 (defn run []
   (first
     (filter
-      #(> (count (gen-factors %)) 500)
+      #(> (num-divisors %) 500)
+      ;#(> (count (gen-factors %)) 500)
       triangle-numbers)))
